7c8b8331c3fb44131bdabb6aa21a7b1be63f15fd
fatih <fthdrop3@gmail.com>
2025-05-19 01:30:13 +0200

    b

---
+++ Kelime oyunu/Assets/WordChef/_Scripts/Main/GameLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class GameLevel : ScriptableObject
+{
+	public string arananlar;
+	public string word;
+    public string answers;
+    public string validWords;
+	
+}
